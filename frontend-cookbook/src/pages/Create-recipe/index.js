8906a334f7497dc159961e866bb7729621465349
Marob-nod <robin.marien@hotmail.fr>
2022-01-19 16:18:24 +0100

    HTML recipe page done + all fields create a recipe page done

--- frontend-cookbook/src/pages/Create-recipe/index.js
+++ frontend-cookbook/src/pages/Create-recipe/index.js
@@ -8,13 +8,22 @@ const CreateRecipe = () => (
         <Header />
         <h1 className='title-page'>Proposes nous ta recette</h1>
         <form className='createrecipe_container_form' action="">
-            <TextField className="form-items" label="Titre de la recette" variant="outlined" />
-            <TextField className="form-items" label="Cooking Time" variant="outlined" />
-            <TextField className="form-items" label="Preparation Time" variant="outlined" />
-            <TextField className="form-items" label="Image" variant="outlined" />
-            <TextField className="form-items" label="Instructions" variant="outlined" />
-            <TextField className="form-items" label="Ingredients" variant="outlined" />
-            <ButtonCustom name="Submit" className="secondary" />
+            <h1><TextField className="form-items" label="Titre de la recette" variant="outlined" /></h1>
+            <div>
+                <TextField className="form-items" label="Cooking Time" variant="outlined" />
+                <TextField className="form-items" label="Preparation Time" variant="outlined" />
+                <TextField className="form-items" label="Serves" variant="outlined" />
+                <TextField className="form-items" label="Level" variant="outlined" />
+            </div>
+            <div>
+                <TextField className="form-items" label="Image" variant="outlined" />
+                <TextField className="form-items" label="Instructions" variant="outlined" />
+                <TextField className="form-items" label="Ingredients" variant="outlined" />
+                <ButtonCustom name="Submit" className="secondary" />
+            </div>
+
+
+
         </form>
     </div>
 );
--- frontend-cookbook/src/pages/Recipe/index.js
+++ frontend-cookbook/src/pages/Recipe/index.js
@@ -1,6 +1,7 @@
 import './styles.scss';
 import { useParams } from "react-router-dom";
 import dataRecipes from '../../public/data/recipes'
+import Header from '../../components/Header'
 
 const RecipePage = () => {
 
@@ -12,8 +13,24 @@ const RecipePage = () => {
 
     return (
         <div className='recipepage_container'>
-            <h1>Recipe: {params.recipeSlug}</h1>
-            <h2>{currentRecipe.slug}</h2>
+            <Header />
+            <h1>{currentRecipe.name}</h1>
+            <ul className="recipepage_container_specifications">
+                <li className="recipepage_container_specifications_item">{currentRecipe.cookTime}</li>
+                <li className="recipepage_container_specifications_item">{currentRecipe.prepTime}</li>
+                <li className="recipepage_container_specifications_item">{currentRecipe.serves}</li>
+                <li className="recipepage_container_specifications_item">{currentRecipe.level}</li>
+            </ul>
+            <ul className="recipepage_container_ingredients">
+                {currentRecipe.ingredients.map((ingredient, index) => (
+                    <li className="recipepage_container_ingredients_ingredient" key={index}>{ingredient}</li>
+                ))}
+            </ul>
+            <ul className="recipepage_container_instructions">
+                {currentRecipe.instructions.map((instruction, index) => (
+                    <li className="recipepage_container_instructions_instruction" key={index}>{instruction.text}</li>
+                ))}
+            </ul>
         </div>
     );
 }
