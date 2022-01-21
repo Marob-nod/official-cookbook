7e592ab9feb40ed12c313bdbec74dc1b9e842126
Marob-nod <robin.marien@hotmail.fr>
2022-01-21 15:07:37 +0100

    post request doesnt work

--- frontend-cookbook/src/components/Button-customized/index.js
+++ frontend-cookbook/src/components/Button-customized/index.js
@@ -1,9 +1,9 @@
 import './styles.scss';
 
 
-const ButtonCustom = ({ name, className }) => {
+const ButtonCustom = ({ name, className, handleClick }) => {
     return (
-        <div className={className}>
+        <div className={className} onClick={handleClick}>
             {name}
         </div>
     );
--- frontend-cookbook/src/pages/Create-recipe/index.js
+++ frontend-cookbook/src/pages/Create-recipe/index.js
@@ -1,31 +1,84 @@
 import './styles.scss';
-import TextField from '@mui/material/TextField'
 import Header from '../../components/Header';
 import ButtonCustom from '../../components/Button-customized';
+import { useState } from 'react';
+import axios from 'axios'
 
-const CreateRecipe = () => (
-    <div className='createrecipe_container'>
-        <Header />
-        <h1 className='title-page'>Proposes nous ta recette</h1>
-        <form className='createrecipe_container_form' action="">
-            <h1><TextField className="form-items" label="Titre de la recette" variant="outlined" /></h1>
-            <div>
-                <TextField className="form-items" label="Cooking Time" variant="outlined" />
-                <TextField className="form-items" label="Preparation Time" variant="outlined" />
-                <TextField className="form-items" label="Serves" variant="outlined" />
-                <TextField className="form-items" label="Level" variant="outlined" />
-            </div>
-            <div>
-                <TextField className="form-items" label="Image" variant="outlined" />
-                <TextField className="form-items" label="Instructions" variant="outlined" />
-                <TextField className="form-items" label="Ingredients" variant="outlined" />
-                <ButtonCustom name="Submit" className="secondary" />
-            </div>
+const CreateRecipe = () => {
+
+    const optionsAxios = {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+    }
+
+    const [titleRecipe, setTitleRecipe] = useState('')
+    const [serves, setServes] = useState('')
+    const [level, setLevel] = useState('')
+    const [imgUrl, setImgUrl] = useState('')
+    const [cookingTime, setCookingTime] = useState(null)
+    const [prepTime, setPrepTime] = useState(null)
+    const [instructions, setInstructions] = useState([])
+    const [ingredients, setIngredients] = useState([])
+    const [isSubmitted, setIsSubmitted] = useState(false)
 
 
+    const handleSubmit = (e) => {
+        //e.prevent.default()
+        setIsSubmitted(true)
+        createEvent();
+    }
 
-        </form>
-    </div>
-);
+    const createEvent = () => {
+        console.log('Le body de la requete create recipe : ', {
+            "title": titleRecipe,
+            "cookingTime": cookingTime,
+            "prepTime": prepTime,
+            "serves": serves,
+            "level": level,
+            "ingredients": ingredients,
+            "instructions": instructions,
+        })
+        axios
+            .post(
+                "http://localhost:3000/recipes",
+                {
+                    "title": "recette test",
+                    "cookingTime": "2",
+                    "prepTime": "20",
+                    "serves": "4",
+                    "level": "medium",
+                    "ingredients": "ingredients",
+                    "instructions": "instructions",
+                },
+                optionsAxios
+            )
+            .then((response) => {
+                console.log("réponse du serveur à ma création de recette :", response)
+            })
+            .catch((error) => console.log("Error de create event"));
+    };
+
+    return (
+        <div className='createrecipe_container'>
+            <Header />
+            <h1 className='title-page'>Proposes nous ta recette</h1>
+            <form className='createrecipe_container_form'>
+                <h1><input type="text" autoComplete='off' className='form-input' placeholder="Title" value={titleRecipe} onChange={(e) => setTitleRecipe(e.target.value)} /></h1>
+                <div>
+                    <input className='form-input' placeholder="Cooking Time" value={cookingTime} onChange={(e) => setCookingTime(e.target.value)} />
+                    <input className='form-input' placeholder="Preparation Time" value={prepTime} onChange={(e) => setPrepTime(e.target.value)} />
+                    <input className='form-input' placeholder="Number of Serves" value={serves} onChange={(e) => setServes(e.target.value)} />
+                    <input className='form-input' placeholder="Level" value={level} onChange={(e) => setLevel(e.target.value)} />
+                </div>
+                <div>
+                    <input className='form-input' placeholder="Image Url" value={imgUrl} onChange={(e) => setImgUrl(e.target.value)} />
+                    <input className='form-input' placeholder="Instructions" value={instructions} onChange={(e) => setInstructions(e.target.value)} />
+                    <input className='form-input' placeholder="Ingredients" value={ingredients} onChange={(e) => setIngredients(e.target.value)} />
+                </div>
+                <ButtonCustom name="Submit" className="secondary" handleClick={handleSubmit} />
+            </form>
+        </div>
+    );
+}
 
 export default CreateRecipe; 
