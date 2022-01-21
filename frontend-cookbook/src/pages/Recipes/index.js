f02f0d00ac91f7ddc321561955e263b09a205626
Marob-nod <robin.marien@hotmail.fr>
2022-01-21 13:48:40 +0100

    server, app created - first axios request getAllRecipes works

--- frontend-cookbook/src/pages/Recipes/index.js
+++ frontend-cookbook/src/pages/Recipes/index.js
@@ -1,30 +1,59 @@
 import './styles.scss';
-import dataRecipes from '../../public/data/recipes'
+//import dataRecipes from '../../public/data/recipes'
 import RecipeCard from '../../components/recipe-card';
 import Header from '../../components/Header';
 import { Link, Outlet } from 'react-router-dom';
-import RecipePage from '../Recipe';
+import { useEffect, useState } from 'react';
+import axios from 'axios'
 
-const RecipesPage = () => (
-    <div className='recipespage_container'>
-        <Header />
-        <h1>All recipes Page</h1>
-        <div className="recipespage_container_list">
-            {dataRecipes.map((recipe) => (
-                <Link to={`/recipes/${recipe.slug}`} key={recipe.name}><RecipeCard  {...recipe} /></Link>
-            ))}
-            {dataRecipes.map((recipe) => (
-                <RecipeCard key={recipe.name} {...recipe} />
-            ))}
-            {dataRecipes.map((recipe) => (
-                <RecipeCard key={recipe.name} {...recipe} />
-            ))}
-            {dataRecipes.map((recipe) => (
-                <RecipeCard key={recipe.name} {...recipe} />
-            ))}
+const RecipesPage = () => {
+
+    const [dataRecipes, setDataRecipes] = useState([])
+
+    const optionsAxios = {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+    }
+
+    const getAllEvents = () => {
+        axios
+            .get("http://localhost:3001/recipes", optionsAxios)
+            .then((response) => {
+                console.log("All the recipes: ", response.data)
+                setDataRecipes(() => response.data)
+            })
+            .catch((error) =>
+                console.log("ERREUR : I can't get all the recipes")
+            )
+    };
+
+    useEffect(() => {
+        getAllEvents()
+    }, [])
+
+    console.log('DataRecipes : ', dataRecipes)
+
+    return (
+        <div className='recipespage_container'>
+            <Header />
+            <h1>All recipes Page</h1>
+            <div className="recipespage_container_list">
+                {dataRecipes.map((recipe) => (
+                    <Link to={`/recipes/${recipe.slug}`} key={recipe.name} ><RecipeCard  {...recipe} /></Link>
+                ))}
+                {dataRecipes.map((recipe) => (
+                    <RecipeCard key={recipe.name} {...recipe} />
+                ))}
+                {dataRecipes.map((recipe) => (
+                    <RecipeCard key={recipe.name} {...recipe} />
+                ))}
+                {dataRecipes.map((recipe) => (
+                    <RecipeCard key={recipe.name} {...recipe} />
+                ))}
+            </div>
+            <Outlet />
         </div>
-        <Outlet />
-    </div>
-);
+    );
+}
 
 export default RecipesPage; 
