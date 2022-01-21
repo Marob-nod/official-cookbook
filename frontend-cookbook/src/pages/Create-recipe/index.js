a80b84f69ef3cb28af35bc5c5e45c6a0b983c679
Marob-nod <robin.marien@hotmail.fr>
2022-01-21 18:09:53 +0100

    Choix du level

--- frontend-cookbook/src/pages/Create-recipe/index.js
+++ frontend-cookbook/src/pages/Create-recipe/index.js
@@ -58,6 +58,8 @@ const CreateRecipe = () => {
             .catch((error) => console.log("Error de create event"));
     };
 
+    console.log('valeur de level : ', level)
+
     return (
         <div className='createrecipe_container'>
             <Header />
@@ -65,13 +67,28 @@ const CreateRecipe = () => {
             <form className='createrecipe_container_form'>
                 <h1><input type="text" autoComplete='off' className='form-input' placeholder="Title" value={titleRecipe} onChange={(e) => setTitleRecipe(e.target.value)} /></h1>
                 <div>
-                    <input type="number" className='form-input' placeholder="Cooking Time" value={cookingTime} onChange={(e) => setCookingTime(e.target.value)} />
-                    <input type="number" className='form-input' placeholder="Preparation Time" value={prepTime} onChange={(e) => setPrepTime(e.target.value)} />
-                    <input type="number" className='form-input' placeholder="Number of Serves" value={serves} onChange={(e) => setServes(e.target.value)} />
-                    <input type="text" className='form-input' placeholder="Level" value={level} onChange={(e) => setLevel(e.target.value)} />
+                    <input type="number" className='createrecipe_container_form_form-input' placeholder="Cooking Time" value={cookingTime} onChange={(e) => setCookingTime(e.target.value)} />
+                    <input type="number" className='createrecipe_container_form_form-input' placeholder="Preparation Time" value={prepTime} onChange={(e) => setPrepTime(e.target.value)} />
+                    <input type="number" className='createrecipe_container_form_form-input' placeholder="Number of Serves" value={serves} onChange={(e) => setServes(e.target.value)} />
+                    <div className='createrecipe_container_form_form-input_level'>
+                        <p>Select a level for your recipe</p>
+                        <div className='createrecipe_container_form_form-input_level-easy'>
+                            <input type="radio" name="level" id="levelChoice1" className='form-input' value="easy" onChange={(e) => setLevel(e.target.value)} />
+                            <label for="levelChoice1">Easy</label>
+                        </div>
+                        <div className='createrecipe_container_form_form-input_level-medium'>
+                            <input type="radio" name="level" id="levelChoice2" className='form-input' value="medium" onChange={(e) => setLevel(e.target.value)} />
+                            <label for="levelChoice2">Medium</label>
+                        </div>
+                        <div className='createrecipe_container_form_form-input_level-hard'>
+                            <input type="radio" name="level" id="levelChoice3" className='form-input' value="hard" onChange={(e) => setLevel(e.target.value)} />
+                            <label for="levelChoice3">Hard</label>
+                        </div>
+                    </div>
+
                 </div>
                 <div>
-                    <input type="text" className='form-input' placeholder="Image Url" value={imgUrl} onChange={(e) => setImgUrl(e.target.value)} />
+                    <input type="url" className='form-input' placeholder="Image Url" value={imgUrl} onChange={(e) => setImgUrl(e.target.value)} />
                     <input type="text" className='form-input' placeholder="Instructions" value={instructions} onChange={(e) => setInstructions(e.target.value)} />
                     <input type="text" className='form-input' placeholder="Ingredients" value={ingredients} onChange={(e) => setIngredients(e.target.value)} />
                 </div>
